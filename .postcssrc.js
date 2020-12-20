115ed08eb5ed39a0b7f0520729aeebcf88df9e9c
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-12-19 19:23:10 -0500

    bookeeping

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
@@ -10,27 +8,3 @@ module.exports = {
     "autoprefixer": {}
   }
 }
-=======
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
->>>>>>> dc83ae0ef91f39efdb6bc65b6e3524ed6fa6e146
-=======
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
->>>>>>> dc83ae0ef91f39efdb6bc65b6e3524ed6fa6e146
