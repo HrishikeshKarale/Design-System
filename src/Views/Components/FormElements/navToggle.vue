60221ee1056b7857460e49372b138467e7c5f551
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2021-01-03 17:55:17 -0500

    fixed toggle input

--- src/Views/Components/FormElements/navToggle.vue
+++ src/Views/Components/FormElements/navToggle.vue
@@ -1,157 +1,136 @@
 //https://proto.io/freebies/onoff/
 
 <template>
-    <div class= "navToggle"> 
-            <div class= 'FormElementsTitle'>
-                <div 
-                    v-if= 'd_toggle'
-                    class= 'value'
-                >
-                    <label>Value Returned:</label> {{d_toggle}}
-                </div>
-            </div>
-            <div class= "FormElementsContent">
-                <form>                            <component-details
-                        :compData= 'd_multiToggle'
-                    >
-                        <multi-toggle 
-                            :label= "d_label"
-                            :name= "d_name"
-                            :value= "d_toggle"
-                            :labelChecked= "d_labelChecked"
-                            :labelUnchecked= "d_labelUnchecked"
-                            :showLabels= 'd_booleanTrue'
-                           
-                            
-                            
-                            @value= "val => d_toggle= val"
-                        />
-                    </component-details>
-                </form>
-            </div>
-           </div>
+  <component-details
+		:compData= 'd_multiToggle'
+	>
+		<multi-toggle
+			:label= "d_label"
+			:tag= "d_name"
+			:value= "d_value"
+			:labelChecked= "d_labelChecked"
+			:labelUnchecked= "d_labelUnchecked"
+			:showLabels= 'd_showLabels'
+			:inline= 'd_inline'
+			:readonly= 'd_readonly'
+			:disabled= 'd_disabled'
+			:mask= 'd_mask'
+			@value= "val => (d_value = val)"
+		/>
+	</component-details>
 </template>
 
 <script>
 
-    import componentDetails from "@/Views/componentDetails";
-    import multiToggle from "@/components/FormElements/multiToggle";
-    
+  import componentDetails from "@/Views/componentDetails";
+  import multiToggle from "@/components/FormElements/multiToggle";
 
-    export default {
-        name: "navToggle",
+  export default {
+		name: "navToggle",
 
-        // store,
+    components: {
+      componentDetails,
+      multiToggle
+    }, //components
 
-        components: {
+    data() {
+        const d_label= 'Username'
+        const d_name= 'toggleInputField'
+				const d_labelChecked= "ON"
+				const d_labelUnchecked= "OFF"
+        const d_value= d_labelChecked
+				const d_booleanTrue= true
+				const d_showLabels= !d_booleanTrue
+				const d_inline= !d_booleanTrue
+				const d_readonly= !d_booleanTrue
+				const d_disabled= !d_booleanTrue
+				const d_mask= !d_booleanTrue
+        return {
+          d_label,
+          d_name,
+          d_value,
+          d_value,
+          d_labelChecked,
+          d_labelUnchecked,
+					d_showLabels,
+					d_inline,
+					d_readonly,
+					d_disabled,
+					d_mask
+      } //return
+		}, //data
 
-            componentDetails,
-            multiToggle
-        }, //components
-
-        data() {
-
-                const label= 'Username'
-
-                const name= 'toggleInputField'
-
-                const value= 'qwerty'
-
-                const toggle= null
-
-                const d_booleanTrue= true
-                               const labelChecked= "ON"
-                               const labelUnchecked= "OFF"
-
-                return {
-
-                    d_label: d_label,
-
-                    d_name: d_name,
-
-                    d_value: d_value,
-
-                    d_toggle: d_toggle,
-
-                    d_labelChecked: d_labelChecked,
-
-                    d_labelUnchecked: d_labelUnchecked,
-
-                    d_booleanTrue: d_booleanTrue,
-
-                d_multiToggle: {
-                                       title: 'Multi-Toggle Input Field',
-
-                    compName: 'multi-toggle',
-
-                    import: 'import multiToggle from "@/components/FormElements/multiToggle";',
-
-                    description: '<p>The <mark>&lt;multi-toggle&gt</mark>; tag specifies an input field where user can toggle between two states.</p>',
-
-                    attributes:[
-                        {
-                            type: "label",
-                            value: d_label,
-                            description: this.$store.state.navText.label,
-                            text: this.$store.state.navText.labelText,
-                        },
-                        {
-                            type: "name",
-                            value: d_name,
-                            description: this.$store.state.navText.name,
-                            text: this.$store.state.navText.nameText,
-                        },
-                        {
-                            type: "v-model",
-                            value: 'd_toggle',
-                            description: this.$store.state.navText.vModel,
-                            text: this.$store.state.navText.vModelText,
-                        },
-                        {
-                            type: "value",
-                            value: d_value,
-                            description: this.$store.state.navText.value,
-                            text: "Specifies the value for the &lt;multi-toggle&gt element.",
-                        },
-                        {
-                            type: "labelUnchecked",
-                            value: d_labelUnchecked,
-                            description: '<p>It sets the label for unchecked value if showLabel is true.</p>',
-                            text: 'Specifies the value for the unchecked label'
-                        },
-                        {
-                            type: "labelChecked",
-                            value: d_labelChecked,
-                            description: '<p>It sets the label for checked value if showLabel is true.</p>',
-                            text: 'Specifies the value for the checked label'
-                        },
-                        {
-                            type: "showLabels",
-                            value: d_booleanTrue,
-                            description: '<p>display the label as checked/unchecked state for toggle.</p>',
-                            text: 'Specifies the if the labels should be displayed'
-                        },
-                        {
-                            type: "disabled",
-                            value: !d_booleanTrue,
-                            description: this.$store.state.navText.disabled,
-                            text: this.$store.state.navText.disabledText,
-                        },
-                        {
-                            type: "inline",
-                            value: !d_booleanTrue,
-                            description: this.$store.state.navText.inline,
-                            text: this.$store.state.navText.inlineText,
-                        },
-                        {
-                            type: "mask",
-                            value: !d_booleanTrue,
-                            description: this.$store.state.navText.mask,
-                            text: this.$store.state.navText.maskText,
-                        },
-                    ]
-                }, //multiToggle
-            } //return
-        }, //data
-    }
+		computed: {
+			d_multiToggle: function() {
+				return {
+					title: 'Multi-Toggle Input Field',
+          compName: 'multi-toggle',
+          import: 'import multiToggle from "@/components/FormElements/multiToggle";',
+          description: '<p>The &lt;multi-toggle&gt; tag specifies an input field where user can toggle between two states.</p>',
+          attributes:[
+            {
+              type: "label",
+              value: this.d_label,
+              description: this.$store.state.navText.label,
+              text: this.$store.state.navText.labelText,
+            },
+            {
+              type: "tag",
+              value: this.d_name,
+              description: this.$store.state.navText.name,
+              text: this.$store.state.navText.nameText,
+            },
+            {
+              type: "value",
+              value: this.d_value,
+              description: this.$store.state.navText.value,
+              text: "Specifies the value for the &lt;multi-toggle&gt element.",
+            },
+            {
+              type: "labelUnchecked",
+              value: this.d_labelUnchecked,
+              description: '<p>It sets the label for unchecked value if showLabel is true.</p>',
+              text: 'Specifies the value for the unchecked label'
+            },
+            {
+              type: "labelChecked",
+              value: this.d_labelChecked,
+              description: '<p>It sets the label for checked value if showLabel is true.</p>',
+              text: 'Specifies the value for the checked label'
+            },
+            {
+              type: "showLabels",
+              value: this.d_showLabels,
+              description: '<p>display the label as checked/unchecked state for toggle.</p>',
+              text: 'Specifies the if the labels should be displayed'
+            },
+            {
+              type: "disabled",
+              value: this.d_disabled,
+              description: this.$store.state.navText.disabled,
+              text: this.$store.state.navText.disabledText,
+            },
+            {
+              type: "readonly",
+              value: this.d_readonly,
+              description: this.$store.state.navText.readonly,
+              text: this.$store.state.navText.readonlyText,
+            },
+            {
+              type: "inline",
+              value: this.d_inline,
+              description: this.$store.state.navText.inline,
+              text: this.$store.state.navText.inlineText,
+            },
+            {
+              type: "mask",
+              value: this.d_mask,
+              description: this.$store.state.navText.mask,
+              text: this.$store.state.navText.maskText,
+            },
+          ]
+				};
+			}//multiToggle
+		}
+  }
 </script>
