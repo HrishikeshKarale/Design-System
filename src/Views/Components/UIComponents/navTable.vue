2c2e78dc8a0e8fc19b8a8757fa3741d51be6fa6b
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2021-01-04 00:46:26 -0500

    table needs lot of work

--- src/Views/Components/UIComponents/navTable.vue
+++ src/Views/Components/UIComponents/navTable.vue
@@ -1,162 +1,134 @@
+// https://uxdesign.cc/designing-tables-for-reusability-490a3760533
 <template>
-    <div class= "navTable"> 
-        https://uxdesign.cc/designing-tables-for-reusability-490a3760533<br/>
-            <component-details
-                v-if= "paginatedData"
-                :compData= 'd_table'
-            >
-                <vue-table 
-                    :tableData= 'paginatedData'
-                    :columns= 'd_columns'
-                    :metadata= 'metadata'
-                    :select= 'd_columns[0]'
-                    :title= 'd_title'
-                    :low= 'd_low'
-                    @setPage= 'setPage'
-                    @setSortKey= 'setSortKey'
-                    @selectRow= 'selectRow'
-                    @setRowElements= 'setRowElements'
-                />
-            </component-details>
-           </div><!--uiComponents-->
+  <component-details
+    :compData= 'd_table'
+  >
+    <vue-table
+      :tableData= 'paginatedData'
+      :columns= 'd_columns'
+      :metadata= 'metadata'
+      :select= 'd_columns[0]'
+      :title= 'd_title'
+      :low= 'd_low'
+      @setPage= 'setPage'
+      @setSortKey= 'setSortKey'
+      @selectRow= 'selectRow'
+      @setRowElements= 'setRowElements'
+    />
+  </component-details>
 </template>
 
 <script>
 
-    import vueTable from '@/components/UIComponents/Table/vueTable'
-    import componentDetails from '@/Views/componentDetails'
-    
+  import vueTable from '@/components/UIComponents/Table/vueTable'
+	import componentDetails from '@/Views/componentDetails'
 
-    // import { store } from '@/store/store'
-    import { mapState, mapActions } from 'vuex';
+  import { mapState, mapActions } from 'vuex';
 
-    export default {
-        name: 'navTable',
+  export default {
+    name: 'navTable',
 
-        d_url: 'https://api.myjson.com/bins/qcjt6',
+    d_url: 'https://api.myjson.com/bins/qcjt6',
 
-        // store,
+		mapState,
 
-        mapState,
+    mapActions,
 
-        mapActions,
+    data() {
+      const d_title= 'Table Name'
+			const d_low= '1'
+      const d_columns= [
+        'Order Number',
+        'Status',
+        'Created On',
+        'Created By'
+      ]
+      return {
+        d_columns,
+        d_title,
+        d_low
+      } //return
+    }, //data
 
-        data() {
+    methods: {
+			...mapActions(
+        [
+          'selectRow',
+          'setSortKey',
+          'setPage',
+          'setRowElements',
+        ]
+      ),
+    }, //methods
 
-            const title= 'Table Name'
-                       const low= '1'
+    computed: {
+      ...mapState (
+        [
+          'paginatedData',
+          'metadata',
+          'selectedFilter',
+        ]
+			),
 
-            const table= {
+      d_table: function() {
+				return {
+					title: 'Table',
+					compName: 'vue-table',
+					import: 'import vueTable from "@/components/UIComponents/Table/vueTable"',
+					description: '<p></p>',
+					attributes: [
+						{
+							type: "data",
+							value: this.paginatedData,
+							description: '<p>The tableDetails attribute provides the table with data pertaining to columns adn records for the particulat &lt;vue-table&gt;.</p>\
+											<p>The tableDetails attribute is an Object and has data and cols as keys to hold records and column names respectively.</p>\
+											<p>This is a required field and cannot be left null/empty.</p>',
+							text: 'Specifies the rows and columns for the table as an object'
+						},
+						{
+							type: "columns",
+							value:  this.d_columns,
+							description: '<p>The tableDetails attribute provides the table with data pertaining to columns adn records for the particulat &lt;vue-table&gt;.</p>\
+											<p>The tableDetails attribute is an Object and has data and cols as keys to hold records and column names respectively.</p>\
+											<p>This is a required field and cannot be left null/empty.</p>',
+							text: 'Specifies the rows and columns for the table as an object'
+						},
+						{
+							type: "metadata",
+							value: this.metadata,
+							description: '<p>The tableDetails attribute provides the table with data pertaining to columns adn records for the particulat &lt;vue-table&gt;.</p>\
+											<p>The tableDetails attribute is an Object and has data and cols as keys to hold records and column names respectively.</p>\
+											<p>This is a required field and cannot be left null/empty.</p>',
+							text: 'Specifies the rows and columns for the table as an object'
+						},
+						{
+							type: "title",
+							value: this.d_title,
+							description: '<p>The title attribute provides the table with a title or name for the particulat &lt;vue-table&gt;.</p>\
+											<p>This title attribute take a string input and displays it in the top-left corner.</p>',
+							text: 'Specifies the title/name for the table.'
+						},
+						{
+							type: "low",
+							value: this.d_low,
+							description: '<p>The title attribute provides the table with a title or name for the particulat &lt;vue-table&gt;.</p>\
+											<p>This title attribute take a string input and displays it in the top-left corner.</p>',
+							text: 'Specifies the title/name for the table.'
+						}
+					], //attributes
+				}
+			} //d_table
+    }, //computed
 
-                title: 'Table',
+    beforeCreate () {
 
-                compName: 'vue-table',
+      this.$store.dispatch('initData', 'https://api.myjson.com/bins/qcjt6')
+    }, //beforeCreate
 
-                import: 'import vueTable from "@/components/UIComponents/Table/vueTable"',
+    components: {
 
-                description: '<p></p>',
-
-                attributes: [
-                    {
-                        type: "data",
-                        value: this.paginatedData,
-                        description: '<p>The tableDetails attribute provides the table with data pertaining to columns adn records for the particulat &lt;vue-table&gt;.</p>\
-                                        <p>The tableDetails attribute is an Object and has data and cols as keys to hold records and column names respectively.</p>\
-                                        <p>This is a required field and cannot be left null/empty.</p>',
-                        text: 'Specifies the rows and columns for the table as an object'
-                    },
-                    {
-                        type: "columns",
-                        value:  this.d_columns,
-                        description: '<p>The tableDetails attribute provides the table with data pertaining to columns adn records for the particulat &lt;vue-table&gt;.</p>\
-                                        <p>The tableDetails attribute is an Object and has data and cols as keys to hold records and column names respectively.</p>\
-                                        <p>This is a required field and cannot be left null/empty.</p>',
-                        text: 'Specifies the rows and columns for the table as an object'
-                    },
-                    {
-                        type: "metadata",
-                        value: this.metadata,
-                        description: '<p>The tableDetails attribute provides the table with data pertaining to columns adn records for the particulat &lt;vue-table&gt;.</p>\
-                                        <p>The tableDetails attribute is an Object and has data and cols as keys to hold records and column names respectively.</p>\
-                                        <p>This is a required field and cannot be left null/empty.</p>',
-                        text: 'Specifies the rows and columns for the table as an object'
-                    },
-                    {
-                        type: "title",
-                        value: d_title,
-                        description: '<p>The title attribute provides the table with a title or name for the particulat &lt;vue-table&gt;.</p>\
-                                        <p>This title attribute take a string input and displays it in the top-left corner.</p>',
-                        text: 'Specifies the title/name for the table.'
-                    },
-                    {
-                        type: "low",
-                        value: d_low,
-                        description: '<p>The title attribute provides the table with a title or name for the particulat &lt;vue-table&gt;.</p>\
-                                        <p>This title attribute take a string input and displays it in the top-left corner.</p>',
-                        text: 'Specifies the title/name for the table.'
-                    }
-                ], //attributes
-            } //d_table
-
-            const columns= [
-                'Order Number',
-                'Status',
-                'Created On',
-                'Created By'
-            ]
-
-            return {
-
-                d_columns: d_columns,
-
-                d_title: d_title,
-
-                d_low: d_low,
-                                   d_table: d_table,
-            } //return
-        }, //data
-
-        methods: {
-                       ...mapActions(
-                [
-                    'selectRow',
-                    'setSortKey',
-                    'setPage',
-                    'setRowElements',
-                ]
-            ),
-        }, //methods
-
-        computed: {
-
-            ...mapState (
-                [
-                    'paginatedData',
-                    'metadata',
-                    'selectedFilter',
-                ]
-            ),
-        }, //computed
-
-        beforeCreate () {
-
-            this.$store.dispatch('initData', 'https://api.myjson.com/bins/qcjt6')
-        }, //beforeCreate
-
-        watch: {
-
-            d_data: function (newValue, oldValue) {
-                if (newValue != oldValue) {
-                    // console.log('navTable', newValue.data)
-                    this.d_tableData= newValue
-                }
-            }
-        }, //watch
-
-        components: {
-
-            vueTable,
-            componentDetails,
-                   }, //components
-    } //default
+      vueTable,
+      componentDetails,
+		} //components
+  } //default
 </script>
