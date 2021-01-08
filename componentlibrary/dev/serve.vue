9344aa49bd039f3b573d94484c2c9e929a000e3f
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2021-01-08 14:49:38 -0500

    js-yaml updated

--- componentlibrary/dev/serve.vue
+++ componentlibrary/dev/serve.vue
@@ -4,14 +4,11 @@ import { ComponentLibraryVue2Sample } from '@/entry';
 
 export default Vue.extend({
   name: 'ServeDev',
-  components: {
-    ComponentLibraryVue2Sample,
-  }
 });
 </script>
 
 <template>
   <div id="app">
-    <component-library-vue2-sample />
+  working
   </div>
 </template>
